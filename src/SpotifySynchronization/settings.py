df3250eba4f32f1605ba5b168e9fdb7dd8fdc210
unknown <fatihemreakardere@hotmail.com>
2025-02-03 18:13:17 +0300

    Refactor STATIC_ROOT and STATICFILES_DIRS to use os.path.join for consistency

--- src/SpotifySynchronization/settings.py
+++ src/SpotifySynchronization/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,9 +83,10 @@ TEMPLATES = [
 ]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 WSGI_APPLICATION = 'SpotifySynchronization.wsgi.application'
