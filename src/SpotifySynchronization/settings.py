37cddfa74cf9d0a6b1730e534c91ebd39805eb2a
unknown <fatihemreakardere@hotmail.com>
2025-02-03 18:19:15 +0300

    Add Whitenoise middleware to settings.py for static file handling

--- src/SpotifySynchronization/settings.py
+++ src/SpotifySynchronization/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', #above all middleware except security (static files)
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
